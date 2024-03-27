1d3c457330dd6c3e62569bf545f0cc270e60f39e
Lamba237 <simontanyi2004@gmail.com>
2024-03-27 05:36:19 -0700

    updates

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,15 +10,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	if (tree->left)
-	{
-		binary_tree_delete(tree->left);
-		free(tree->left);
-	}
-	if (tree->right)
-	{
-		binary_tree_delete(tree->right);
-		free(tree->right);
-	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
 	free(tree);
 }
