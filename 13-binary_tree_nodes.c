b09c6e340c2a699b4e586d2f388483a35b0cca24
Lamba237 <simontanyi2004@gmail.com>
2024-03-27 15:04:25 -0700

    updates

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,14 +9,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
-	if (!tree)
-		return (0);
-	if ((tree->left != NULL && tree->right == NULL) ||
-			(tree->left == NULL && tree->right != NULL)) {
-		count++;
+	if (tree)
+	{
+		count += (tree->left || tree->right) ? 1 : 0;
+		count += binary_tree_nodes(tree->left);
+		count += binary_tree_nodes(tree->right);
 	}
-
-	count += binary_tree_nodes(tree->left);
-	count += binary_tree_nodes(tree->right);
 	return (count);
 }
